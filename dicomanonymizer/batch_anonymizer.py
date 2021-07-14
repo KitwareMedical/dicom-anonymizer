42040f39a789e214a12a180b0538fd33988682b5
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-07-14 13:56:04 +0200

    Add debug and cache

--- dicomanonymizer/batch_anonymizer.py
+++ dicomanonymizer/batch_anonymizer.py
@@ -6,6 +6,8 @@
 """
 
 import argparse
+import json
+import random
 from pathlib import Path
 
 from dicomanonymizer.dicom_utils import fix_exposure
@@ -18,15 +20,21 @@ from dicomanonymizer.utils import (
     try_valid_dir,
 )
 
+_STATE_PATH = Path.home() / ".dicomanonymizer/cache"
+create_if_not_exist(_STATE_PATH, parents=True)
+STATE_FILE = _STATE_PATH / "state_cache.json"
 
-def anonymize_dicom_folder(in_path: Path_Str, out_path: Path_Str):
+
+def anonymize_dicom_folder(in_path: Path_Str, out_path: Path_Str, debug: bool = False):
     """Anonymize dicom files in `in_path`, if `in_path` doesn't
-    contain dicom files, will do nothing
+    contain dicom files, will do nothing. Debug == True will do
+    sort of dry run to check if all good for the large data storages
 
     Args:
         in_path (Path_Str): path to the folder containing dicom files
         out_path (Path_Str): path to the folder there anonymized copies
         will be saved
+        debuf (bool): if true, will do a "dry" run
     """
     # check and prepare
     in_path = to_Path(in_path)
@@ -43,12 +51,18 @@ def anonymize_dicom_folder(in_path: Path_Str, out_path: Path_Str):
         print(f"Folder {in_path} doesn't have dicom files, skip.")
         return
 
-    for f_in in in_files:
+    if debug:
+        # anonymize just one file
+        f_in = random.choice(in_files)
         f_out = out_path / f_in.name
         anonymize_dicom_file(f_in, f_out)
+    else:
+        for f_in in in_files:
+            f_out = out_path / f_in.name
+            anonymize_dicom_file(f_in, f_out)
 
 
-def anonymize_root_folder(in_root: Path_Str, out_root: Path_Str):
+def anonymize_root_folder(in_root: Path_Str, out_root: Path_Str, **kwargs):
     """The fuction will get all nested folders from `in_root`
     and perform anonymization of all folders containg dicom-files
     Will recreate the `in_root` folders structure in the `out_root`
@@ -65,10 +79,30 @@ def anonymize_root_folder(in_root: Path_Str, out_root: Path_Str):
     create_if_not_exist(out_root)
     in_dirs = get_dirs(in_root)
 
-    for in_d in in_dirs:
-        rel_path = in_d.relative_to(in_root)
-        out_d = out_root / rel_path
-        anonymize_dicom_folder(in_d, out_d)
+    try:
+        with open(STATE_FILE, "r") as f:
+            processed_state = json.load(f)
+    except FileNotFoundError:
+        processed_state = None
+    if processed_state is None:
+        processed_state = {}
+
+    # will try to process all folders, if exception will dump state before raising
+    try:
+        for in_d in in_dirs:
+            if str(in_d) in processed_state:
+                continue
+            else:
+                rel_path = in_d.relative_to(in_root)
+                out_d = out_root / rel_path
+                anonymize_dicom_folder(in_d, out_d, **kwargs)
+                # update state
+                processed_state[str(in_d)] = str(out_d)
+    except Exception as e:
+        raise e
+    finally:
+        with open(STATE_FILE, "w") as f:
+            json.dump(processed_state, f)
 
 
 # Add CLI args
@@ -76,20 +110,21 @@ parser = argparse.ArgumentParser(description="Batch dicom-anonymization CLI")
 parser.add_argument(
     "--type",
     type=str,
-    required=True,
     choices=["batch", "folder"],
-    help="Process only one folder or all nested folders",
+    default="batch",
+    help="Process only one folder - folder or all nested folders - batch, default = batch",
 )
 parser.add_argument(
-    "--src",
+    "--debug", action="store_true", help="Will do a dry run (one file per folder)"
+)
+parser.add_argument(
+    "src",
     type=str,
-    required=True,
     help="Absolute path to the folder containing dicom-files or nested folders with dicom-files",
 )
 parser.add_argument(
-    "--dst",
+    "dst",
     type=str,
-    required=True,
     help="Absolute path to the folder where to save anonymized copy of src",
 )
 
@@ -98,12 +133,13 @@ def main():
     args = parser.parse_args()
     in_path = Path(args.src)
     out_path = Path(args.dst)
+    debug = args.debug
     # fix known issue with dicom
     fix_exposure()
     if args.type == "batch":
-        anonymize_root_folder(in_path, out_path)
+        anonymize_root_folder(in_path, out_path, debug=debug)
     elif args.type == "folder":
-        anonymize_dicom_folder(in_path, out_path)
+        anonymize_dicom_folder(in_path, out_path, debug=debug)
 
 
 if __name__ == "__main__":
