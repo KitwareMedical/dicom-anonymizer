6a3cfbf9fa5a993b3d868ebafd4ad2d861f2eb6d
sharayujosh <61659558+sharayujosh@users.noreply.github.com>
2023-06-22 21:03:56 -0400

    Wrote test to check deleted tags

--- tests/test_anon.py
+++ tests/test_anon.py
@@ -1,10 +1,17 @@
 from pydicom import dcmread
+from pydicom import tag
 from pydicom.data import get_testdata_file
+from dicomanonymizer import dicomfields
 from dicomanonymizer import anonymize_dataset
 
-def test_pydicom_data():
 
+def test_pydicom_data():
     sample_data = get_testdata_file("CT_small.dcm")
     ds = dcmread(sample_data)
+    original_ds = ds.copy()
 
     anonymize_dataset(ds)
+
+    for tt in dicomfields.X_TAGS:
+        if len(tt) == 2 and tt in original_ds:
+            assert tt not in ds
