2723c3aff88bb52c6ddc7fb232558fddc192961c
sharayujosh <61659558+sharayujosh@users.noreply.github.com>
2023-06-28 09:25:07 -0400

    Wrote test for U tags

--- tests/test_anon.py
+++ tests/test_anon.py
@@ -10,8 +10,16 @@ def test_pydicom_data():
     ds = dcmread(sample_data)
     original_ds = ds.copy()
 
+    #  for tt in dicomfields.Z_TAGS:
+    #     if tt in original_ds:
+    #         assert tt in [0, "", None]
+
     anonymize_dataset(ds)
 
     for tt in dicomfields.X_TAGS:
         if len(tt) == 2 and tt in original_ds:
             assert tt not in ds
+
+    for tt in dicomfields.U_TAGS:
+        if tt in original_ds:
+            assert ds[tt] != original_ds[tt]
