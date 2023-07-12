c262eddcda0f9f1a2243e9e87b058e0799d520a6
sharayujosh <61659558+sharayujosh@users.noreply.github.com>
2023-07-12 18:31:12 -0400

    implemented suggestions

--- tests/test_anon.py
+++ tests/test_anon.py
@@ -1,34 +1,26 @@
-from pydicom import dcmread
 from pydicom.data import get_testdata_file
+import pytest
 from dicomanonymizer import dicomfields
 from dicomanonymizer import anonymize_dataset
 
 
-def test_pydicom_data():
-    sample_data = get_testdata_file("CT_small.dcm")
-    ds = dcmread(sample_data)
-    original_ds = ds.copy()
-
-    anonymize_dataset(ds)
-
-    verify_anonmyzation(original_ds, ds)
+@pytest.fixture(scope="module")
+def CT_small_anonymized_dataset():
+    orig_ds = get_testdata_file("CT_small.dcm", read=True)
+    anon_ds = orig_ds.copy()
+    anonymize_dataset(anon_ds)
+    return (orig_ds, anon_ds)
 
 
-def verify_anonmyzation(orig_ds, anon_ds):
-    verify_deleted_tags(orig_ds, anon_ds)
-    verify_changed_tags(orig_ds, anon_ds)
-    verify_empty_tags(orig_ds, anon_ds)
-
-
-def verify_deleted_tags(orig_ds, anon_ds):
+def test_deleted_tags_are_removed(CT_small_anonymized_dataset):
+    orig_ds, anon_ds = CT_small_anonymized_dataset
     deleted_tags = dicomfields.X_TAGS
-
     for tt in deleted_tags:
         if len(tt) == 2 and tt in orig_ds:
             assert tt not in anon_ds
 
 
-def verify_changed_tags(orig_ds, anon_ds):
+def test_changed_tags_are_replaced(CT_small_anonymized_dataset):
     changed_tags = []
     changed_tags.extend(dicomfields.U_TAGS)
     changed_tags.extend(dicomfields.D_TAGS)
@@ -37,16 +29,20 @@ def verify_changed_tags(orig_ds, anon_ds):
     changed_tags.extend(dicomfields.X_Z_D_TAGS)
     changed_tags.extend(dicomfields.X_Z_U_STAR_TAGS)
 
+    orig_ds, anon_ds = CT_small_anonymized_dataset
+
     for tt in changed_tags:
         if tt in orig_ds:
             assert anon_ds[tt] != orig_ds[tt]
 
 
-def verify_empty_tags(orig_ds, anon_ds):
+def test_empty_tags_are_emptied(CT_small_anonymized_dataset):
     empty_values = (0, "", "00010101", "000000.00")
     empty_tags = []
     empty_tags.extend(dicomfields.Z_TAGS)
     empty_tags.extend(dicomfields.X_Z_TAGS)
+
+    orig_ds, anon_ds = CT_small_anonymized_dataset
 
     for tt in empty_tags:
         if tt in orig_ds:
